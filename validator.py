fe3777682583ce7b1f536d57d7093553f5246c80
Mert-Gunes <91959456+Mert-Gunes@users.noreply.github.com>
2024-06-29 00:54:43 +0300

    Update validator.py
    Validator took its last shape and delivered.

--- validator.py
+++ validator.py
@@ -2,8 +2,8 @@ class PDA:
     def __init__(self):
         self.stack = []
         self.state = "q0"
-        self.valid_operators = {'+', '-', '*', '/'}
-
+        self.operatorler = {'+', '-', '*', '/'}
+    
     def transition(self, char):
         if self.state == "q0":
             if char.isdigit():
@@ -15,7 +15,7 @@ class PDA:
                 self.state = "error"
 
         elif self.state == "q1":
-            if char in self.valid_operators:
+            if char in self.operatorler:
                 self.state = "q2"
             elif char == ')':
                 if self.stack and self.stack[-1] == '(':
@@ -36,24 +36,32 @@ class PDA:
                 self.state = "q0"
             else:
                 self.state = "error"
-
+    
     def process_string(self, input_string):
-        for char in input_string:
+        prev_char = ""
+        for i, char in enumerate(input_string):
             self.transition(char)
             if self.state == "error":
                 return False
+            # Sağ parantezden sonra rakam gelmemesi kontrolü
+            if prev_char == ')' and char.isdigit():
+                return False
+            # Bölme operatöründen sonra sıfır olup olmadığını kontrol et
+            if prev_char == '/' and char == '0':
+                return False
+            prev_char = char
 
         if self.stack:
             return False
-        
+
         return self.state == "q1"
 
 def validate_expression(expression):
     pda = PDA()
     return pda.process_string(expression)
 
-# Test cases
-expressions = [
+# Testler
+ifadeler = [
     "(1+2)*(3/4)",
     "((2+3)*(4-1))",
     "(1+2*(3/4)",
@@ -61,8 +69,15 @@ expressions = [
     "1+2)*3",
     "(1+2)*(3/4",
     "1+2",
-    "((1+2)*3)/4"
+    "((1+2)*3)/0",
+    "1/0+2",
+    "(1+2)/0",
+    "(1+0)/0",
+    "(2)2+2",
+    "((4)/ 2)",
+    "(4/2)-8",
+    "(3)"
 ]
 
-for expr in expressions:
-    print(f"{expr}: {'Valid' if validate_expression(expr) else 'Invalid'}")
+for ifade in ifadeler:
+    print(f"{ifade}: {'Tanir' if validate_expression(ifade) else 'Tanimaz'}")
